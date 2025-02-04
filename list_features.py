d52866928c30186ecc5d28f99ceb40b8c0501d9d
Seth Tenenbaum <sethtenenbaum@Seths-MacBook-Pro.local>
2025-02-04 15:57:03 -0500

    list available labels app

--- list_features.py
+++ list_features.py
@@ -11,5 +11,7 @@ with open('features/labels.pkl', 'rb') as f:
     print("Labels loaded:", labels.shape)
 
 # Get a list of all unique labels
-unique_labels = list(set(labels))
-print(f"Unique labels: {unique_labels}")
+unique_labels = list(set(tuple(label) for label in labels))
+print("Unique labels:")
+for label in unique_labels:
+    print(label)
